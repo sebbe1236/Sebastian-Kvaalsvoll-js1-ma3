8cdf4838e499e1abb516079e5cecd43e99027976
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2021-04-15 16:22:40 +0200

    soon there. number of tags left

---
+++ js/question -1.js
@@ -0,0 +1,3 @@
+const division = () => {
+  return a % b;
+};
---
+++ js/question-2.js
@@ -0,0 +1,26 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+const display = document.querySelector(".display-results");
+
+async function gamesResults() {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    const gamesTotal = result.results;
+
+    display.innerHTML = "";
+    for (let i = 0; i < gamesTotal.length; i++) {
+      if (i === 8) {
+        break;
+      }
+      display.innerHTML += `<div>${gamesTotal[i].name}</div>
+        <div>${gamesTotal[i].rating}</div>
+        <div>${gamesTotal.length}</div>
+        `;
+    }
+  } catch (error) {
+    display.innerHTML = `<div> An error happened</div>`;
+  }
+}
+
+gamesResults();
